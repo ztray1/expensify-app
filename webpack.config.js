5917aa8a8463345efb864444e6ea42cdeeab5375
ztray1 <zhangtuo332@gmail.com>
2020-11-10 11:56:24 -0500

    Setup production webpack build

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,14 @@
-//entry ->output
-
 const path=require("path");
+const ExtractTextPlugin=require("extract-text-webpack-plugin");
 
 console.log(path.join(__dirname,"public"));
 
-module.exports={
-    entry:"./src/app.js",
+module.exports=(env)=>{
+    const isProduction= env==="production";
+    const CSSExtract=new ExtractTextPlugin("styles.css");
+    console.log("env",env);
+    return{
+        entry:"./src/app.js",
     output:{
         path:path.join(__dirname,"public"),
         filename:"bundle.js"
@@ -17,16 +20,31 @@ module.exports={
             exclude:/node_modules/
         },{
             test:/\.s?css$/,
-            use:[
-                "style-loader",
-                "css-loader",
-                "sass-loader"
-            ]
+            use:CSSExtract.extract({
+                use:[   
+                    {
+                        loader:"css-loader",
+                        options:{
+                            sourceMap:true
+                        }
+                    },
+                    {
+                        loader:"sass-loader",
+                        options:{
+                            sourceMap:true
+                        }
+                    }
+                ]
+            })
         }]
     },
-    devtool:"cheap-module-eval-source-map",
+    plugins:[
+        CSSExtract
+    ],
+    devtool:isProduction?"source-map":"inline-source-map",
     devServer:{
         contentBase:path.join(__dirname,"public"),
         historyApiFallback:true
     }
+    };
 };
