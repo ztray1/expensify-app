3d8cfd17149c6d960d5820f1f12ac8a6e4963870
ztray1 <zhangtuo332@gmail.com>
2020-11-10 18:41:12 -0500

    deploying on herku

--- src/components/ExpenseDashboardPage.js
+++ src/components/ExpenseDashboardPage.js
@@ -1,6 +1,6 @@
 import React from "react";
 import ExpenseList from "./ExpenseList";
-import ExpenselistFilters from "./ExpenselistFilters";
+import ExpenselistFilters from "./ExpenseListFilters";
 
 const ExpenseDashboardPage=()=>(
     <div>
