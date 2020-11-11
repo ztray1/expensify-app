6cc9867ebf4a34178f287007362973d67f7fb921
ztray1 <zhangtuo332@gmail.com>
2020-11-10 19:01:39 -0500

    heroku deploying 1

--- server/server.js
+++ server/server.js
@@ -1,6 +1,5 @@
 const path=require("path");
 const express=require("express");
-const { publicEncrypt } = require("crypto");
 const app=express();
 const publicPath=path.join(__dirname,"..","public");
 const port=process.env.PORT||3000;
